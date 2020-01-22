8c0fbf85832a064f672ef33f742b16854b20107f
Richard Paredes <richard_olivares286@hotmail.com>
2020-01-22 00:03:45 -0500

    Implenting user roles
    It was implement role user privileges in User model

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,6 +1,17 @@
 class EducationsController < ApplicationController
 	before_action :authenticate_user!
-	
-	def index
+
+	before_action :superuser_only
+
+  def index
+
 	end
+
+
+private
+
+def superuser_only
+  redirect_to root_path unless current_user.superuser?
+end 
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { superuser: 0, admin: 1, costumer: 2 }
 end
